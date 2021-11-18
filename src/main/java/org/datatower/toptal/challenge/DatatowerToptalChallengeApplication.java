05e18e1479db21c2c0d4f8975d30e08848e23257
Guga Tkesheladze <guram.tkesheladze@azry.com>
2021-11-18 16:42:47 +0400

    initial commit - created skeleton of the app

---
+++ src/main/java/org/datatower/toptal/challenge/DatatowerToptalChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.datatower.toptal.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatowerToptalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatowerToptalChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/datatower/toptal/challenge/DatatowerToptalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datatower.toptal.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatatowerToptalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
